8111e2a918c83da8c5ac8f0ba95f47bd1fa8be5e
nchung14 <nchung14@punahou.edu>
2014-02-25 14:12:03 -1000

    inDanger method progress
    The inDanger piece method has been theoretically completed. It however
    only works if used on black (AI) pieces which from a design standpoint
    may be a bit flawed but for our purposes is fine so far. I've made it
    so it differentiates between Kings and Pieces to recognized danger by
    more than just proximity.

--- Piece.java
+++ Piece.java
@@ -1,5 +1,7 @@
 // Nolan Chung
-//
+// Piece class code to be integrated with Jaz's Piece class
+
+// ONLY WORKS FOR BLACK PIECES
 
 import info.gridworld.grid.Grid;
 import info.gridworld.actor.Actor;
@@ -16,17 +18,34 @@ public class Piece extends Bug
   {
     boolean output = false;
     for(int i = 0; i < neighbors.size() - 1; i++)
-    {   
+    {
       Location playerPieceLoc = neighbors.get(i).getLocation();
       int oppositeDirection = loc.getDirectionToward(playerPieceLoc) + 180;
       if(oppositeDirection >= 360)
       {
         oppositeDirection = oppositeDirection - 360;
       }
-      Location oppositeDirectionLocation = loc.getAdjacentLocation(oppositeDirection);
-
-      if(getGrid().get(oppositeDirectionLocation) == null)
-        output = true;      
+      Location oppositeDirectionLocation = loc.getAdjacentLocation(oppositeDirection); 
+      if(neighbors.get(i) instanceof WhiteKing)
+      {
+        if(getGrid().get(oppositeDirectionLocation) == null)
+        {
+          output = true;
+          return output;
+        }
+      }
+      if(neighbors.get(i) instanceof WhitePiece)
+      {
+        int pieceDirection = neighbors.get(i).getDirection();
+        if(pieceDirection == 0 || pieceDirection == 45 || pieceDirection == 315)
+        {
+          if(getGrid().get(oppositeDirectionLocation) == null)
+          {
+            output = true;
+            return output;
+          }
+        }
+      }
     }
     return output;
   }
